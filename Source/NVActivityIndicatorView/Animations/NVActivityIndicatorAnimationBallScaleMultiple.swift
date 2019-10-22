68b8bbda95dc96b0176ea4028c3b776a854d2a7b
Vu Ho <hohaivu@gmail.com>
2019-10-22 10:03:22 +0700

    Update NVActivityIndicatorAnimationBallScaleMultiple.swift
    22/10/2019 - STABLE Increase BallScaleMultiple to 2s

--- Source/NVActivityIndicatorView/Animations/NVActivityIndicatorAnimationBallScaleMultiple.swift
+++ Source/NVActivityIndicatorView/Animations/NVActivityIndicatorAnimationBallScaleMultiple.swift
@@ -28,9 +28,8 @@
 import UIKit
 
 class NVActivityIndicatorAnimationBallScaleMultiple: NVActivityIndicatorAnimationDelegate {
-
     func setUpAnimation(in layer: CALayer, size: CGSize, color: UIColor) {
-        let duration: CFTimeInterval = 1
+        let duration: CFTimeInterval = 2
         let beginTime = CACurrentMediaTime()
         let beginTimes = [0, 0.2, 0.4]
 
